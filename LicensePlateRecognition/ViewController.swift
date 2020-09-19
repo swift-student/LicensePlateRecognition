9d859cb4e371ea49e8e388546c91b015cfb6423c
Shawn Gee <shawngee@gmail.com>
2020-09-19 10:11:23 -0400

    add Xcode project

---
+++ LicensePlateRecognition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LicensePlateRecognition
+//
+//  Created by Shawn Gee on 9/19/20.
+//  Copyright © 2020 Swift Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
