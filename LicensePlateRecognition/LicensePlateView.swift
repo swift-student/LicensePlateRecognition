1179585fcdc642f9a32bad8f5112149b3b297920
Shawn Gee <shawngee@gmail.com>
2020-09-20 01:55:37 -0400

    set up basic license plate view

--- LicensePlateRecognition/LicensePlateView.swift
+++ LicensePlateRecognition/LicensePlateView.swift
@@ -9,5 +9,21 @@
 import UIKit
 
 class LicensePlateView: UIView {
+    var licensePlate: LicensePlate?
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUp()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setUp()
+    }
+    
+    private func setUp() {
+        layer.borderColor = UIColor.red.cgColor
+        layer.borderWidth = 2.0
+        layer.cornerRadius = 6.0
+    }
 }
