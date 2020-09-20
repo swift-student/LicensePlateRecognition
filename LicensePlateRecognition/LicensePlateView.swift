ef1807a6095f8ab9ad856aaa6972b5bf557fd259
Shawn Gee <shawngee@gmail.com>
2020-09-20 00:50:48 -0400

    add license plate view

---
+++ LicensePlateRecognition/LicensePlateView.swift
@@ -0,0 +1,13 @@
+//
+//  LicensePlateView.swift
+//  LicensePlateRecognition
+//
+//  Created by Shawn Gee on 9/20/20.
+//  Copyright © 2020 Swift Student. All rights reserved.
+//
+
+import UIKit
+
+class LicensePlateView: UIView {
+    
+}
