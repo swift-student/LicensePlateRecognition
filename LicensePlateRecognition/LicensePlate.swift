15474006d3f532d6434fe93fb57099a58da5f0e2
Shawn Gee <shawngee@gmail.com>
2020-09-20 22:06:50 -0400

    fix license plate equatable conformance

--- LicensePlateRecognition/LicensePlate.swift
+++ LicensePlateRecognition/LicensePlate.swift
@@ -15,7 +15,11 @@ struct LicensePlate {
     let uuid = UUID()
 }
 
-extension LicensePlate: Equatable {}
+extension LicensePlate: Equatable {
+    static func == (lhs: LicensePlate, rhs: LicensePlate) -> Bool {
+        lhs.uuid == rhs.uuid
+    }
+}
 extension LicensePlate: Hashable {
     func hash(into hasher: inout Hasher) {
         hasher.combine(uuid)
